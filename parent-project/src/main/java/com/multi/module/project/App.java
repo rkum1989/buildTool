6791aa1018f93e33561aa8c4224e12c4cbf11c8a
RI342338 <RI342338@L-156133713.wipro.com>
2018-03-10 23:20:13 +0530

    Maven Multi Module POC

---
+++ parent-project/src/main/java/com/multi/module/project/App.java
@@ -0,0 +1,13 @@
+package com.multi.module.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
